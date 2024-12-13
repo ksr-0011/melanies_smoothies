167cc42d9823b971bc8eb1cf5d09ddc2a01d3ed3
Krishna Singh <96103885+ksr-0011@users.noreply.github.com>
2024-12-13 22:26:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
         st.success(f'Your Smoothie is ordered!, {name_on_order}!', icon="✅")
 
 # New section to display smoothiefront nutrition info
-smoothiefroot_response = requests.get("https:/my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
 sf_df = st.datafram(data=smoothiefroot_response.json(), use_container_width=True)
 
